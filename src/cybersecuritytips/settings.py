a751d1577dd27dbbfef2ca902306b6121238b67f
Sofia Di Nonno <sofidinonno@gmail.com>
2023-01-20 08:50:46 -0300

    update settings

--- src/cybersecuritytips/settings.py
+++ src/cybersecuritytips/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!^d#1t!qla$9n98p#6xm7%5_uv3y=17$5#e4mse@pn9&_w_^l'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
