82858cb604bf238030de1cd18141056206759c1f
Sofia Di Nonno <sofidinonno@gmail.com>
2023-02-20 00:24:59 -0300

    new models for pages

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tip, TipDescription
 
-# Register your models here.
+
+admin.site.register(Tip)
+admin.site.register(TipDescription)
--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tip(models.Model):
+    text = models.TextField(verbose_name='Tip title')
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+
+class TipDescription(models.Model):
+    question = models.ForeignKey(
+        Tip, related_name='tip', on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='Tip description')
