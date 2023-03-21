f8bad7b9ff74e6b63ebc0fb2cc402ff19e062a03
Sofia Di Nonno <sofidinonno@gmail.com>
2023-03-20 21:01:44 -0300

    update location for pages app

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from hitcount.models import HitCountMixin, HitCount
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Tip(models.Model):
@@ -12,3 +14,6 @@ class TipDescription(models.Model):
     question = models.ForeignKey(
         Tip, related_name='tip', on_delete=models.CASCADE)
     text = models.TextField(verbose_name='Tip description')
+
+class Category(models.Model):
+    text = models.TextField(verbose_name='Category')
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hitcount.views import HitCountDetailView
+
 
 def homepage_view(request, *args, **kwargs):
     return render(request, "home.html", {})
+
 
 def alertpage_view(request, *args, **kwargs):
     return render(request, "alert.html", {})
