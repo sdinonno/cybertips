15c21260da149f7bcbfee94ca80cb9383456f182
Sofía Di Nonno <sofidinonno@gmail.com>
2023-03-24 19:49:45 +0000

    Update DEBUG and add SECRET_KEY configuration

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
