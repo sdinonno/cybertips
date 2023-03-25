737e8cf6cbccc93886869c09ee51afe9b4fa3698
Sofía Di Nonno <sofidinonno@gmail.com>
2023-03-25 22:51:36 +0000

    update allowed hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.122.90.229', 'cybertips.site', 'www.cybertips.site']
 
 # Application definition
 
